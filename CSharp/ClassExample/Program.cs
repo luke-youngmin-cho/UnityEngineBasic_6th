92a3c29de731a50aea876a2d05986a5c812b8e50
YoungMin Cho (Luke) <83196845+luke-youngmin-cho@users.noreply.github.com>
2022-12-14 21:46:45 +0900

    C# - Class
    1. ClassObjectInstance
    - Member access operator
    - class  vs object  vs instance
    
    2. Class Example
    - Created simple test

---
+++ CSharp/ClassExample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ClassExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human human1 = new Human();
+            human1.Age = 100;
+            human1.Height = 200.0f;
+            human1.Gender = '남';
+
+            Human human2 = new Human();
+            human2.Age = 50;
+            human2.Height = 150.0f;
+            human2.Gender = '여';
+
+            human1.SayAge();
+            human2.SayAge();
+        }
+    }
+
+    class Human
+    {
+        public int Age;
+        public float Height;
+        public char Gender;
+
+
+        public void SayAge()
+        {
+            Console.WriteLine(Age);
+        }
+    }
+}
