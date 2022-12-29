7addc3d75ebc292abde5ab9e19c8d491ed7e26c8
YoungMin Cho (Luke) <83196845+luke-youngmin-cho@users.noreply.github.com>
2022-12-29 21:54:58 +0900

    C# - BinaryTree, Collections
    1. BinaryTree
    - Completed
    
    2. Collections
    - Usage of some collections classes in System.Collections & System.Collections.Generic

--- CSharp/BinaryTree/Program.cs
+++ CSharp/BinaryTree/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace BinaryTree
 {
@@ -6,7 +7,30 @@ namespace BinaryTree
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MyBinaryTree<int> tree = new MyBinaryTree<int>();
+
+            tree.Add(5);
+            tree.Add(3);
+            tree.Add(4);
+            tree.Add(7);
+            tree.Add(6);
+            tree.Add(9);
+            tree.Add(10);
+            tree.Add(8);
+
+            Console.WriteLine(tree.Root.Value); // 5
+            Console.WriteLine(tree.Root.Left.Value); // 3
+            Console.WriteLine(tree.Root.Left.Right.Value); // 4
+            Console.WriteLine(tree.Root.Right.Value); // 7
+            Console.WriteLine(tree.Root.Right.Left.Value); // 6
+            Console.WriteLine(tree.Root.Right.Right.Value); // 9
+            Console.WriteLine(tree.Root.Right.Right.Left.Value); // 8
+            Console.WriteLine(tree.Root.Right.Right.Right.Value); // 10
+
+            tree.Remove(7);
+            Console.WriteLine(tree.Root.Right.Value); // 8
+            Console.WriteLine(tree.Root.Right.Right.Value); // 9
+            Console.WriteLine(tree.Root.Right.Right.Right.Value); // 9
         }
     }
 }
