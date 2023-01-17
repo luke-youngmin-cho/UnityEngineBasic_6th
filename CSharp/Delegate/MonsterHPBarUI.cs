12176825551c6e0fbec659dda4dee80434e51d38
YoungMin Cho (Luke) <83196845+luke-youngmin-cho@users.noreply.github.com>
2023-01-17 21:43:22 +0900

    C# - Delegate
    Delegate
    - Simple example
    - basic delegates of C#
    - event
    - Anonymous delegate, methods ( + Lambda expression)
    - How to make observable collection

---
+++ CSharp/Delegate/MonsterHPBarUI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    internal class MonsterHPBarUI
+    {
+        public float barValue; // 0.0f ~ 1.0f
+
+        public MonsterHPBarUI(Monster owner)
+        {
+            owner.OnHPChange += Refresh;
+        }
+
+        public void Refresh(float value) => barValue = value;
+    }
+}
--- CSharp/Delegate/Program.cs
+++ CSharp/Delegate/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 // Delegate : 대리자 
 // Methods 에 대한 참조를 나타내는 형식
@@ -10,8 +11,25 @@ namespace Delegate
         public delegate int OPHandler(int a, int b);
         public static OPHandler op;
 
+        // C# 기본 대리자들
+        public Action action;
+        public Action<int> action2;
+        public Action<float, float> action3;
+        public Func<int, int, int> op2;
+        public Predicate<int> predicate;
+
+
         static void Main(string[] args)
-        {           
+        {
+            Monster monster = new Monster();
+            MonsterHPBarUI monsterHPBarUI = new MonsterHPBarUI(monster);
+            
+            
+            while (true)
+            {
+            }
+
+            //===============================================================
             int result = 0;
             Sum(1, 2);
             Sub(1, 2);
@@ -23,7 +41,23 @@ namespace Delegate
             op += Div;
             op += Mul;
 
-            op(1, 2);
+            op(1, 2); 
+            op.Invoke(1, 2); // 일반호출과 Invoke호출의 차이는 멀티쓰레드환경에서 레이스컨디션을 방지하기위한 함수
+            op?.Invoke(1, 2); // ? : null check 연산자. null 이면 호출안함
+
+            // 익명 대리자 추가 형태
+            op += delegate (int a, int b)
+            {
+                return a + b + b + a;
+            };
+
+            // 익명 메소드 추가 형태 (람다식)
+            // 굳이 명시하지않아도 컴파일러가 판단할수 있는 내용들을 전부 생략하고 => 로 람다식표현이라는것만 명시하는 형태로 사용
+            op += (a, b) =>
+            {
+                return a + b + b + a;
+            };
+
         }
 
         static int Sum(int a, int b)
